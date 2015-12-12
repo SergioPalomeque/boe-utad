b81a5fecb2e70d4837089c513c2e40574c3caf17
Sergio <sergio.palomeque@questionity.com>
2015-12-12 20:55:10 +0100

    first commit

---
+++ src/main/scala/boe/BoeApp.scala
@@ -0,0 +1,66 @@
+package boe
+
+import java.sql.{ResultSet, DriverManager}
+import java.text.Normalizer
+import java.util.regex.Pattern
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.JdbcRDD
+import org.jsoup.Jsoup
+
+import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
+
+object BoeApp extends App {
+
+  /*
+   * Pametrizar palabras validas. Apartir de 3
+   * Limpia el texto, minúsculas, sin tildes ni signos de puntuación como coma, punto. Los caracteres alfanuméricos y números se eliminan
+   * Filtrar palabras que no esten el la lista stopwords
+   * Crear mega matriz. La fila es el documento. La columna las palabras del documento. El valor sera el numero de veces que esta ese documento
+   */
+
+  val url = "192.168.1.38"
+  val port = "5432"
+  val database = "boe"
+  val user = "sergio"
+  val password = "postgres"
+
+  val stopWordsList = List("algún","alguna","algunas","alguno","algunos","ambos","ampleamos","ante","antes","aquel","aquellas","aquellos","aqui","arriba","atras","bajo","bastante","bien","cada","cierta","ciertas","cierto","ciertos","como","con","conseguimos","conseguir","consigo","consigue","consiguen","consigues","cual","cuando","dentro","desde","donde","dos","el","ellas","ellos","empleais","emplean","emplear","empleas","empleo","en","encima","entonces","entre","era","eramos","eran","eras","eres","es","esta","estaba","estado","estais","estamos","estan","estoy","fin","fue","fueron","fui","fuimos","gueno","ha","hace","haceis","hacemos","hacen","hacer","haces","hago","incluso","intenta","intentais","intentamos","intentan","intentar","intentas","intento","ir","la","largo","las","lo","los","mientras","mio","modo","muchos","muy","nos","nosotros","otro","para","pero","podeis","podemos","poder","podria","podriais","podriamos","podrian","podrias","por","que","porque","primero","puede","pueden","puedo","quien","sabe","sabeis","sabemos","saben","saber","sabes","ser","si","siendo","sin","sobre","sois","solamente","solo","somos","soy","su","sus","tambie","teneis","tenemos","tener","tengo","tiempo","tiene","tienen","todo","trabaja","trabajais","trabajamos","trabajan","trabajar","trabajas","trabajo","tras","tuyo","ultimo","un","una","unas","uno","unos","usa","usais","usamos","usan","usar","usas","uso","va","vais","valor","vamos","van","vaya","verdad","verdadera","verdadero","vosotras","vosotros","voy","yo")
+
+  val sc = new SparkContext("local", "Simple", "$SPARK_HOME")
+
+  def createConnection () = {
+    DriverManager.getConnection("jdbc:postgresql://"+ url +":"+ port +"/"+ database +"?user="+ user +"&password="+ password)
+  }
+
+  def removeAccent(str: String): String = {
+    val nfdNormalizedString = Normalizer.normalize(str, Normalizer.Form.NFD)
+    val pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+")
+    pattern.matcher(nfdNormalizedString).replaceAll("")
+  }
+
+  def extractValues (r: ResultSet): BoeDocument = {
+    var doc = Jsoup.parse(r.getString("texto")).text()
+    doc = removeAccent(doc)
+    val textWords = doc.replaceAll("[^a-zA-Z ]", "").toLowerCase() //.split("\\s+")
+    new BoeDocument(r.getString("identificador"), textWords)
+  }
+
+  /*def getWords (x: BoeDocument): (String, String) = {
+    val id = x.getId
+    val document = x.getText
+    document.foreach(x => (id, x))
+  }*/
+
+  val query = "SELECT identificador, texto FROM boe_analisis_documento LIMIT ? OFFSET ?"
+  val data = new JdbcRDD(sc, createConnection, query, lowerBound = 1, upperBound = 1000, numPartitions = 2, mapRow = extractValues)
+
+  //println(data.collect().toList)
+
+  val documents = data.collect()
+    .flatMap(x => x.getText.split(" ").map(y => (x.getId, y)))
+    .filter(x => {x._2.length >= 3 && !stopWordsList.contains(x._2)})
+    .foreach(x => println(x))
+}
